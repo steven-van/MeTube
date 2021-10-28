33f8257971ee487aa23944f5ad9c180295256918
steven-van <64859587+steven-van@users.noreply.github.com>
2021-10-29 01:43:19 +0200

    feat: update app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
-import Sidebar from "components/Sidebar/Sidebar";
+import React, { useState } from "react";
+import Sidebar from "components/Sidebar";
+import ThemeContext from "contexts/ThemeContext";
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(() => true);
+
   return (
-    <div className="h-screen bg-gray-800">
-      <Sidebar />
-    </div>
+    <ThemeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
+      <div className={`h-screen ${isDarkMode ? "bg-gray-800" : "bg-gray-200"}`}>
+        <Sidebar />
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
