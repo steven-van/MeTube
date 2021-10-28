24c97927029f860e9bdaa4fc2df162530122fa0a
steven-van <64859587+steven-van@users.noreply.github.com>
2021-10-28 19:15:34 +0200

    feat: searchbar and logo

---
+++ src/components/UI/CentralPayLogo/CentralPayLogo.js
@@ -0,0 +1,7 @@
+import centralPayLogoWhite from "assets/centralpay-logo-white.png";
+
+const CentralPayLogo = () => {
+  return <img alt="CentralPay Logo White" src={centralPayLogoWhite} />;
+};
+
+export default CentralPayLogo;
