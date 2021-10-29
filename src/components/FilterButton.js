4eea00165d8484c8699e6cda66dd36112981ed2f
steven-van <64859587+steven-van@users.noreply.github.com>
2021-10-29 18:22:42 +0200

    feat: dropdown

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,25 +1,69 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import FilterIcon from "components/UI/FilterIcon";
 import ThemeContext from "contexts/ThemeContext";
 
 const FilterButton = React.forwardRef(({}, ref) => {
   const { isDarkMode } = useContext(ThemeContext);
+  const [isDropdown, setDropdown] = useState(false);
+  const handleClick = () => {
+    setDropdown(!isDropdown);
+  };
   return (
-    <button
-      ref={ref}
-      className={`flex w-full justify-center items-center ${
-        isDarkMode ? "bg-gray-800" : "bg-gray-300"
-      } px-2 py-2 rounded-xl space-x-2`}
-    >
-      <div className="w-5">
-        <FilterIcon />
-      </div>
-      <p
-        className={`${isDarkMode ? "text-white" : "text-gray-500"} font-medium`}
+    <div className="relative">
+      <button
+        ref={ref}
+        onClick={handleClick}
+        className={`flex w-full justify-center items-center ${
+          isDarkMode ? "bg-gray-800" : "bg-gray-300"
+        } px-2 py-2 rounded-xl space-x-2`}
       >
-        Filter
-      </p>
-    </button>
+        <div className="w-5">
+          <FilterIcon />
+        </div>
+        <p
+          className={`${
+            isDarkMode ? "text-white" : "text-gray-500"
+          } font-medium`}
+        >
+          Filter
+        </p>
+      </button>
+      {isDropdown && (
+        <div
+          className={`w-full mt-1 rounded-md absolute z-30 ${
+            isDarkMode ? "bg-gray-800" : "bg-gray-300"
+          }`}
+        >
+          <div
+            className={`${
+              isDarkMode ? "text-white" : "text-gray-500"
+            } font-medium`}
+          >
+            <button
+              className={`block w-full text-left px-4 py-1 rounded-t-md hover:bg-gray-600 ${
+                isDarkMode ? "hover:bg-gray-600" : "hover:bg-red-200"
+              }`}
+            >
+              Videos
+            </button>
+            <button
+              className={`block w-full text-left px-4 py-1 hover:bg-gray-600 ${
+                isDarkMode ? "hover:bg-gray-600" : "hover:bg-red-200"
+              }`}
+            >
+              Channels
+            </button>
+            <button
+              className={`block w-full text-left px-4 py-1 pb-1 hover:bg-gray-600 ${
+                isDarkMode ? "hover:bg-gray-600" : "hover:bg-red-200"
+              }`}
+            >
+              Playlists
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
   );
 });
 
--- src/components/OrderByButton.js
+++ src/components/OrderByButton.js
@@ -1,25 +1,58 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import OrderByIcon from "components/UI/OrderByIcon";
 import ThemeContext from "contexts/ThemeContext";
 
 const OrderByButton = React.forwardRef(({}, ref) => {
   const { isDarkMode } = useContext(ThemeContext);
+  const [isDropdown, setDropdown] = useState(false);
+  const handleClick = () => {
+    setDropdown(!isDropdown);
+  };
   return (
-    <button
-      ref={ref}
-      className={`flex w-full justify-center items-center ${
-        isDarkMode ? "bg-gray-800" : "bg-gray-300"
-      } px-2 py-2 rounded-xl space-x-2`}
-    >
-      <div className="w-5">
-        <OrderByIcon />
-      </div>
-      <p
-        className={`${isDarkMode ? "text-white" : "text-gray-500"} font-medium`}
+    <div className="relative">
+      <button
+        ref={ref}
+        onClick={handleClick}
+        className={`flex w-full justify-center items-center ${
+          isDarkMode ? "bg-gray-800" : "bg-gray-300"
+        } px-2 py-2 rounded-xl space-x-2`}
       >
-        Order by
-      </p>
-    </button>
+        <div className="w-5">
+          <OrderByIcon />
+        </div>
+        <p
+          className={`${
+            isDarkMode ? "text-white" : "text-gray-500"
+          } font-medium`}
+        >
+          Order by
+        </p>
+      </button>
+      {isDropdown && (
+        <div
+          className={` w-full mt-1 rounded-md absolute z-20 ${
+            isDarkMode ? "bg-gray-800" : "bg-gray-300"
+          }`}
+        >
+          <div className={`${isDarkMode ? "text-white" : "text-gray-500"}`}>
+            <button
+              className={`block w-full text-left px-4 py-1 rounded-t-md ${
+                isDarkMode ? "hover:bg-gray-600" : "hover:bg-red-200"
+              }`}
+            >
+              Number of views
+            </button>
+            <button
+              className={`block w-full text-left px-4 py-1 rounded-b-md ${
+                isDarkMode ? "hover:bg-gray-600" : "hover:bg-red-200"
+              }`}
+            >
+              Upload date
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
   );
 });
 
