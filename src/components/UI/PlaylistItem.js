f923ac796cb921a71e6925c0e34539b98d378fd3
steven-van <64859587+steven-van@users.noreply.github.com>
2021-11-02 15:05:30 +0100

    feat: link to playlist

--- src/components/UI/PlaylistItem.js
+++ src/components/UI/PlaylistItem.js
@@ -1,15 +1,18 @@
 import React, { useContext } from "react";
 import ThemeContext from "contexts/ThemeContext";
 
-const VideoItem = ({ video }) => {
+const PlaylistItem = ({ video }) => {
   const { isDarkMode } = useContext(ThemeContext);
+  const playlistLink = `https://www.youtube.com/playlist?list=${video.id.playlistId}`;
   return (
     <div className="w-80 h-44 xl:w-96 xl:h-56">
-      <img
-        className="rounded w-full h-full"
-        src={video.snippet.thumbnails.medium.url}
-        alt={video.snippet.description}
-      />
+      <a href={playlistLink} target="_blank" rel="noopener noreferrer">
+        <img
+          className="rounded w-full h-full"
+          src={video.snippet.thumbnails.medium.url}
+          alt={video.snippet.description}
+        />
+      </a>
 
       <p
         className={`overflow-ellipsis overflow-hidden whitespace-nowrap font-medium ${
@@ -22,4 +25,4 @@ const VideoItem = ({ video }) => {
   );
 };
 
-export default VideoItem;
+export default PlaylistItem;
