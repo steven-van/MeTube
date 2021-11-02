da2fe6fae6dd664253e8adbba40731839b60fe15
steven-van <64859587+steven-van@users.noreply.github.com>
2021-11-02 01:58:39 +0100

    fix: change contentcontext data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React, { useState } from "react";
 import Sidebar from "components/Sidebar";
 import ThemeContext from "contexts/ThemeContext";
-import VideoContext from "contexts/VideoContext";
-import VideoSection from "components/VideoSection";
+import ContentContext from "contexts/ContentContext";
+import ContentSection from "components/ContentSection";
 import youtube from "apis/youtube";
+import SearchContext from "contexts/SearchContext";
 
 const App = () => {
   const [isDarkMode, setIsDarkMode] = useState(() => true);
-  const [state, setState] = useState({ videos: [] });
+  const [state, setState] = useState({ contents: [], videosStats: [] });
+  const [contentType, setContentType] = useState("video");
 
-  const getVideos = async (searchTerm) => {
-    const response = await youtube.get("/search", {
+  const getContents = async (searchTerm, type = "video") => {
+    const responseSnippet = await youtube().get("/search", {
       params: {
         q: searchTerm,
+        type: type,
       },
     });
 
-    setState({ videos: response.data.items });
-    console.log(response.data.items);
+    console.log(responseSnippet.data.items);
+
+    if (type === "video") {
+      const videoIds = responseSnippet.data.items
+        .map((item) => {
+          return item.id.videoId;
+        })
+        .join(",");
+
+      const responseStats = await youtube("statistics").get("videos", {
+        params: {
+          id: videoIds,
+        },
+      });
+
+      console.log("hello");
+
+      setState((prevState) => ({
+        ...prevState,
+        videosStats: responseStats.data.items,
+      }));
+    }
+
+    setState((prevState) => ({
+      ...prevState,
+      contents: responseSnippet.data.items,
+    }));
   };
+
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <ThemeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
@@ -27,10 +57,14 @@ const App = () => {
           isDarkMode ? "bg-gray-800" : "bg-gray-200"
         }`}
       >
-        <VideoContext.Provider value={{ state, setState, getVideos }}>
-          <Sidebar />
-          <VideoSection />
-        </VideoContext.Provider>
+        <ContentContext.Provider
+          value={{ state, setState, getContents, contentType, setContentType }}
+        >
+          <SearchContext.Provider value={{ searchTerm, setSearchTerm }}>
+            <Sidebar />
+          </SearchContext.Provider>
+          <ContentSection />
+        </ContentContext.Provider>
       </div>
     </ThemeContext.Provider>
   );
